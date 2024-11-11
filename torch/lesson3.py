309d79295f2ca6ba044bbc3538a1d805ab0f4715
shen <shenhao189@foxmail.com>
2024-11-11 16:10:52 +0800

    pytorch practice

--- torch/lesson3.py
+++ torch/lesson3.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+# y = 2 * x
 x_data = [1.0, 2.0, 3.0]
 y_data = [2.0, 4.0, 6.0]
 
@@ -23,3 +24,22 @@ def gradient(xs,ys):
     for x,y in zip(xs,ys):
         grad += 2 * x * (x * w - y)
     return grad / len(xs)
+
+
+epoch_list = []
+cost_list = []
+print('predict (before training)', 4, forward(4))
+for epoch in range(100):
+    cost_val = cost(x_data, y_data)
+    grad_val = gradient(x_data, y_data)
+    w -= 0.01 * grad_val  # 0.01 learning rate
+    print('epoch:', epoch, 'w=', w, 'loss=', cost_val)
+    epoch_list.append(epoch)
+    cost_list.append(cost_val)
+
+print('predict (after training)', 4, forward(4))
+plt.plot(epoch_list, cost_list)
+plt.ylabel('cost')
+plt.xlabel('epoch')
+plt.show()
+
