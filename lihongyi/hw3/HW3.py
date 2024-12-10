e37a6c0b03c71a0a4427e21fe8fb51fdfb4a1583
shen <shenhao189@foxmail.com>
2024-12-10 16:39:59 +0800

    HW

---
+++ lihongyi/hw3/HW3.py
@@ -0,0 +1,25 @@
+# Import necessary packages.
+import numpy as np
+import pandas as pd
+import torch
+import os
+import torch.nn as nn
+import torchvision.transforms as transforms
+from PIL import Image
+# "ConcatDataset" and "Subset" are possibly useful when doing semi-supervised learning.
+from torch.utils.data import ConcatDataset, DataLoader, Subset, Dataset
+from torchvision.datasets import DatasetFolder, VisionDataset
+
+# This is for the progress bar.
+from tqdm.auto import tqdm
+import random
+
+
+
+myseed = 6666  # set a random seed for reproducibility
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+np.random.seed(myseed)
+torch.manual_seed(myseed)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed_all(myseed)
